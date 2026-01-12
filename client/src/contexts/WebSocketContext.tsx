40cf7f855c6f1ea65128e64f218d9025945381a8
Skye Gibney <astraliskye@gmail.com>
2026-01-11 19:11:34 -0800

    Fix websocket bug

--- client/src/contexts/WebSocketContext.tsx
+++ client/src/contexts/WebSocketContext.tsx
@@ -1,95 +1,91 @@
 import { useMeQuery } from "@hooks/useMeQuery";
 import { createContext, ReactNode, useCallback, useEffect, useRef, useState } from "react";
 
-const websocketUrl = import.meta.env.PROD
-    ? "wss://finar.skyegibney.com"
-    : "";
-
 type WebSocketContext = {
-    send: (message: { gameId?: string, lobbyId?: string, type: string, data?: any }) => void,
-    setMessageCallback: (messageCallback: (message: MessageEvent) => void) => void,
-    connected: boolean
+  send: (message: { gameId?: string, lobbyId?: string, type: string, data?: any }) => void,
+  setMessageCallback: (messageCallback: (message: MessageEvent) => void) => void,
+  connected: boolean
 }
 
 export const WebSocketContext = createContext<WebSocketContext>({
-    send: () => { },
-    setMessageCallback: () => { },
-    connected: false
+  send: () => { },
+  setMessageCallback: () => { },
+  connected: false
 });
 
 type WebSocketProviderProps = {
-    children: ReactNode
+  children: ReactNode
 }
 
 export const WebSocketProvider = ({ children }: WebSocketProviderProps) => {
-    const socketRef = useRef<WebSocket | null>(null);
-    const {
-        isLoading: authLoading,
-        isError: authError
-    } = useMeQuery();
-    const [connected, setConnected] = useState(false);
-    const [reconnecting, setReconnecting] = useState(false);
+  const socketRef = useRef<WebSocket | null>(null);
+  const {
+    isLoading: authLoading,
+    isError: authError
+  } = useMeQuery();
+  const [connected, setConnected] = useState(false);
+  const [reconnecting, setReconnecting] = useState(false);
 
-    const connect = useCallback(() => {
-        if (socketRef.current === null && !reconnecting && !authLoading && !authError) {
-            let intervalId = 0;
+  const connect = useCallback(() => {
+    if (socketRef.current === null && !reconnecting && !authLoading && !authError) {
+      const intervalId = 0;
 
-            const newSocket = new WebSocket(`${websocketUrl}/api/ws`);
+      const newSocket = new WebSocket(`/api/ws`);
 
-            newSocket.onopen = () => {
-                setInterval(() => {
-                    newSocket.send(JSON.stringify({ type: "heartbeat" }));
-                }, 2000);
-                setConnected(true);
-                setReconnecting(false);
-            }
+      newSocket.onopen = () => {
+        setInterval(() => {
+          newSocket.send(JSON.stringify({ type: "heartbeat" }));
+        }, 2000);
+        setConnected(true);
+        setReconnecting(false);
+      }
 
-            newSocket.onclose = () => {
-                setReconnecting(false);
-                clearInterval(intervalId);
-                setConnected(false);
-                setReconnecting(true);
-                socketRef.current = null;
+      newSocket.onclose = () => {
+        setReconnecting(false);
+        clearInterval(intervalId);
+        setConnected(false);
+        setReconnecting(true);
+        socketRef.current = null;
 
-                setTimeout(() => {
-                    setReconnecting(false);
-                    connect();
-                }, 1000);
-            }
+        setTimeout(() => {
+          setReconnecting(false);
+          connect();
+        }, 1000);
+      }
 
-            newSocket.onerror = (error) => {
-                console.error(error);
-            }
+      newSocket.onerror = (error) => {
+        console.error(error);
+      }
 
-            socketRef.current = newSocket;
+      socketRef.current = newSocket;
+    }
+  }, [authLoading, authError]);
+
+  useEffect(() => {
+    connect();
+
+    return () => {
+      socketRef.current?.close();
+    };
+  }, [authLoading, authError]);
+
+  return (
+    <WebSocketContext.Provider value={{
+      send: (message) => {
+        if (socketRef.current) {
+          socketRef.current.send(JSON.stringify(message));
         }
-    }, [authLoading, authError]);
-
-    useEffect(() => {
-        connect();
-
-        return () => {
-            socketRef.current?.close();
-        };
-    }, [authLoading, authError]);
-
-    return (
-        <WebSocketContext.Provider value={{
-            send: (message) => {
-                if (socketRef.current) {
-                    socketRef.current.send(JSON.stringify(message));
-                }
-            },
-            setMessageCallback: (messageCallback: (message: MessageEvent) => void) => {
-                if (socketRef.current) {
-                    socketRef.current.onmessage = messageCallback
-                }
-            },
-            connected
-        }}>
-            {children}
-        </WebSocketContext.Provider >
-    )
+      },
+      setMessageCallback: (messageCallback: (message: MessageEvent) => void) => {
+        if (socketRef.current) {
+          socketRef.current.onmessage = messageCallback
+        }
+      },
+      connected
+    }}>
+      {children}
+    </WebSocketContext.Provider >
+  )
 }
 
 export default WebSocketProvider;
